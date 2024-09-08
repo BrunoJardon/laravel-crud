c25ee1f58b2f082e99b46a8a43a3d83122dac8c7
Bruno Jardon <brunojardon3@gmail.com>
2024-09-08 17:18:42 -0300

    createTask working

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Task;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController {
     public function getTasks() {
@@ -12,7 +13,7 @@ class TaskController {
 
         if($tasks -> isEmpty()){
             $data = [
-                'message' => 'No hay tareas pendiente',
+                'message' => 'You dont have any task',
                 'status' => 200
             ];
 
@@ -26,8 +27,42 @@ class TaskController {
         return "Task ".$id;
     }
 
-    public function createTask() {
-        return "Create task";
+    public function createTask(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:25',
+            'description' => 'max:255',
+            'done' => 'required|boolean'
+        ]);
+
+        if($validator->fails()){
+            $data = [
+                'message' => 'Error at validate',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+
+        $task = Task::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'done' => $request->done
+        ]);
+
+        if(!$task){
+            $data = [
+                'message' => 'Error at create the task',
+                'status' => 500
+            ];
+            return response()->json($data, 500);
+        }
+
+        $data = [
+            'task' => $task,
+            'status' => 201
+        ];
+
+        return response()->json($data, 201);
     }
     
     public function editTask($id) {
