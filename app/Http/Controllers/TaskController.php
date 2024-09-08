4ec6fa2aa507e04f8d1f5e1d9f1b076b03a7c1ba
Bruno Jardon <brunojardon3@gmail.com>
2024-09-08 16:52:34 -0300

    getTasks working

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,25 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TaskController
-{
-    public function getTasks(){
-        return "Tasks lists";
+use App\Models\Task;
+
+class TaskController {
+    public function getTasks() {
+        $tasks = Task::all();
+
+        if($tasks -> isEmpty()){
+            $data = [
+                'message' => 'No hay tareas pendiente',
+                'status' => 200
+            ];
+
+            return response()->json($data, 200);
+        }
+
+        return response()->json($tasks, 200);
     }
 
-    public function getTaskById($id){
+    public function getTaskById($id) {
         return "Task ".$id;
     }
 
-    public function createTask(){
+    public function createTask() {
         return "Create task";
     }
     
-    public function editTask($id){
+    public function editTask($id) {
         return "Task ".$id." edited";
     }
 
-    public function deleteTask($id){
+    public function deleteTask($id) {
         return "Task ".$id." deleted";
     }
 }
